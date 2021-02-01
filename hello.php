64504ab169b9e7a3b9a13d231306e4c782ee710a
noguyu0215 <77600848+noguyu0215@users.noreply.github.com>
2021-02-01 10:46:21 +0900

    Create hello.php

---
+++ hello.php
@@ -0,0 +1,12 @@
+<html lang="ja">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=Shift_Jis">
+		<title> PHP Test Page </title>
+	</head>
+	
+	<body>
+<p>today、<?php echo date("Y/m/d"); ?> </p>
+		
+		
+	</body>
+</html>
