75272fd6a744e848309b1fdbe4c1f04f5483895c
noguyu0215 <77600848+noguyu0215@users.noreply.github.com>
2021-02-03 08:14:07 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">
+<html lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html>
+<meta http-equiv="Pragma" content="no-cache">
+<meta charset="utf-8">
+
+<title>日付の表示/PHP入門サンプル</title>
+
+</head>
+
+<body>
+<div class="link"><a href="../php_03.html#back">BackPage</a></div>
+<h1 id="header">PHPサンプル・日付の表示</h1>
+<dl>
+<dt><span>■</span> 日付を表示します。</dt>
+<dd>今日は、<?php echo date("Y/m/d"); ?> です。</dd>
+</dl><hr>
+
+<p id="footer"><a href="../php_03.html#back">前のページに戻る</a></p><hr>
+<div id="body2">
+最新更新日 2004/1/12
+<address>Copyright(C) 2002-2003 banban@scollabo.com</address>
+</div>
+
+</body>
+</html>
